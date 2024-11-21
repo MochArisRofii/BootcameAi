49e9eed65d3d4bca97324f0d79e48cde539f161c
Moch Aris Rofii <mochrofii827@gmail.com>
2024-11-21 11:27:52 +0700

    Tugas Project

---
+++ Components/tasks.py
@@ -0,0 +1,21 @@
+from crewai import Task
+from Llms import GPTLlm
+from Agents import *
+
+class Tasks:
+    def __init__(self, topik):
+        self.topik = topik
+        
+        
+        def cari_informasi(self):
+            return Task(
+                deskription="Mencari informasi tentang topik tertentu",
+                expected_output="Informasi yang diinginkan",
+                agent=Agents().tren_searcher(), 
+            )
+        def buat_blog(self):
+            return Task(
+                deskription="Membuat blog tentang topik tertentu",
+                expected_output="Blog yang diinginkan",
+                agent=Agents().penulis(),
+            )
